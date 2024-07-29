6eeea175b4c985c05973d7d45cb50fafbb15a859
luispablovelasco <pablo.velascoflores@gmail.com>
2024-07-28 21:52:06 -0600

    Ejercicio 2 FINALIZADO

---
+++ ejercicio2/src/app/data.js
@@ -0,0 +1,112 @@
+// data.js
+export const data = [
+    {
+      id: 1,
+      title: 'Esterilla de Yoga',
+      desc: 'Esterilla antideslizante de alta calidad para yoga y pilates.',
+      urlImage: 'https://contents.mediadecathlon.com/m8976310/k$d94116c2697aead2337a121b37adc664/sq/bfcf2fca-62f5-4d89-a5ea-c39b65bb5228_c16.jpg?format=auto&f=800x0',
+      price: 35,
+      quantity: 1,
+      category: 'Yoga y pilates'
+    },
+    {
+      id: 2,
+      title: 'Bloque de Yoga',
+      desc: 'Bloque de espuma de alta densidad para soporte y equilibrio en yoga.',
+      urlImage: 'https://walmartsv.vtexassets.com/arquivos/ids/332875/Bloque-De-Yoga-Atheltic-Works-1-2290.jpg?v=638295465820100000',
+      price: 35,
+      quantity: 1,
+      category: 'Yoga y pilates'
+    },
+    {
+      id: 3,
+      title: 'Balón de Fútbol',
+      desc: 'Balón de fútbol profesional para entrenamientos y partidos.',
+      urlImage: 'https://resources.sears.com.mx/medios-plazavip/mkt/62acbb5bcf774_img_17062022_123410_-800_x_800_p-xel-png.jpg?scale=500&qlty=75',
+      price: 30,
+      quantity: 1,
+      category: 'Futbol'
+    },
+    {
+      id: 4,
+      title: 'Botas de Fútbol',
+      desc: 'Botas de fútbol de alta calidad para mejor rendimiento en el campo.',
+      urlImage: 'https://media.futbolmania.com/media/catalog/product/cache/1/image/0f330055bc18e2dda592b4a7c3a0ea22/I/D/ID4322_botas-de-futbol-color-negro-adidas-copa-pure-4-fxg_1_pie-derecho.jpg',
+      price: 30,
+      quantity: 1,
+      category: 'Futbol'
+    },
+    {
+      id: 5,
+      title: 'Mochila Deportiva Grande',
+      desc: 'Mochila espaciosa con compartimentos múltiples para equipo deportivo.',
+      urlImage: 'https://clikstore.com/images/amazonasn_2.jpg',
+      price: 50,
+      quantity: 1,
+      category: 'Mochilas deportivas'
+    },
+    {
+      id: 6,
+      title: 'Mochila Deportiva Compacta',
+      desc: 'Mochila ligera y compacta ideal para llevar lo esencial a cualquier competición.',
+      urlImage: 'https://assets.adidas.com/images/w_600,f_auto,q_auto/3dab7c02a35b4dc7aebbaf410128888d_9366/Mochila_Deportiva_Tiro_League_Multicores_HS9768_01_standard.jpg',
+      price: 50,
+      quantity: 1,
+      category: 'Mochilas deportivas'
+    },
+    {
+      id: 7,
+      title: 'Casco de Ciclismo',
+      desc: 'Casco de ciclismo aerodinámico con ventilación eficiente.',
+      urlImage: 'https://m.media-amazon.com/images/I/61qNjgMBWML.jpg',
+      price: 25,
+      quantity: 1,
+      category: 'Ciclismo'
+    },
+    {
+      id: 8,
+      title: 'Guantes de Ciclismo',
+      desc: 'Guantes acolchados para ciclismo, ofreciendo comodidad y agarre.',
+      urlImage: 'https://uno-sports-site.s3.amazonaws.com/uploads/img/products/420cf6fc-261a-48a5-93af-7b34bda82735.jpg',
+      price: 25,
+      quantity: 1,
+      category: 'Ciclismo'
+    },
+    {
+      id: 9,
+      title: 'Guantes de Boxeo',
+      desc: 'Guantes de boxeo profesionales para entrenamiento y competición.',
+      urlImage: 'https://m.media-amazon.com/images/I/61PDHaIEykL.jpg',
+      price: 20,
+      quantity: 1,
+      category: 'Boxeo'
+    },
+    {
+      id: 10,
+      title: 'Saco de Boxeo',
+      desc: 'Saco de boxeo resistente para todo tipo de entrenamiento de golpes y patadas.',
+      urlImage: 'https://viktorsports.com/wp-content/uploads/2020/04/sacoboxeo1.jpg',
+      price: 20,
+      quantity: 1,
+      category: 'Boxeo'
+    },
+    {
+        id: 11,
+        title: 'Pelota de Yoga',
+        desc: 'Pelota de goma, ideal parar realizar ejercicios de yogao simplemente para jugar con la familia',
+        urlImage: 'https://m.media-amazon.com/images/I/614Nf9N7CdL._AC_UF1000,1000_QL80_.jpg',
+        price: 35,
+        quantity: 1,
+        category: 'Yoga y pilates'
+      },
+      {
+        id: 12,
+        title: 'Camiseta de Ciclismo',
+        desc: 'Camiseta técnica de ciclismo con tejidos transpirables y ajuste perfecto.',
+        urlImage: 'https://www.sheffy.co/pub/media/catalog/product/cache/9bb9d677791f8666003e194c8a94aeff/s/h/sheffy_5__1.jpg',
+        price: 35,
+        quantity: 1,
+        category: 'Ciclismo'
+      }
+];
+  
